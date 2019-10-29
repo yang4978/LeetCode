e04a0b3628e2363981524afd1f4cd755f1f53387
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-10-29 11:43:19 +0100

    Update 0046. Permutations.py

--- Python3/0046. Permutations.py
+++ Python3/0046. Permutations.py
@@ -12,4 +12,25 @@ class Solution:
         output = []
         backtrack()
         return output
-                
+    ######         DFS          #####
+    def permute(self, nums: List[int]) -> List[List[int]]:                
+        n = len(nums)
+        result = []
+        temp = []
+        used = [False]*n
+        length = 0
+        self.dfs(nums,temp,used,result,length,n)
+        return result
+    
+    def dfs(self,nums,temp,used,result,length,n):
+        if(length==n):
+            result.append(temp[:])
+            return
+        
+        for i in range(n):
+            if not used[i]:
+                temp.append(nums[i])
+                used[i] = True
+                self.dfs(nums,temp,used,result,length+1,n)
+                used[i] = False
+                temp.pop()
