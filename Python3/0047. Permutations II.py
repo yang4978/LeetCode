91e972cc23195840b49b84aa9c6c1da0597459be
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-11-17 17:50:29 +0100

    Update 0047. Permutations II.py

--- Python3/0047. Permutations II.py
+++ Python3/0047. Permutations II.py
@@ -27,5 +27,33 @@ class Solution:
         dfs(nums,temp,res,used,l,n)
         
         return res
-        
-        
+    
+#     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+#         def dfs(i,nums,temp,res,used,l,n):
+#             if(l==n):
+#                 res.add(temp)
+#                 return
+#             for j in range(n):
+#                 if used[j]!=True:
+#                     tt = temp[:]
+#                     temp += ' '+nums[j]
+#                     used[j] = True
+#                     l += 1
+#                     dfs(j,nums,temp,res,used,l,n)
+#                     used[j] = False
+#                     temp = tt[:]
+#                     l -= 1
+
+#         nums = list(map(str,nums))
+#         res = set()
+#         n = len(nums)
+#         used = [False]*n
+#         temp = ''
+#         l = 1
+#         for i in range(n):
+#             temp = nums[i]
+#             used[i] = True
+#             dfs(i,nums,temp,res,used,l,n)
+#             used[i] = False
+
+#         return([list(map(int,i.split(" "))) for i in res])
