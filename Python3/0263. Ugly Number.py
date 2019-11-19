3c7ad6b31725b30b7fb0ceb6824fd8954fa2bc67
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-11-19 18:12:56 +0100

    Create 0263. Ugly Number.py

---
+++ Python3/0263. Ugly Number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isUgly(self, num: int) -> bool:
+        if(num<=0):
+            return False
+
+        while num>1:
+            t = num
+            if num%2==0:
+                num//=2
+            if num%3==0:
+                num//=3
+            if num%5==0:
+                num//=5
+            
+            if t==num:
+                return False
+            
+        return True
+        # if num<=0:
+        #     return False
+        # while num>1:
+        #     if num%2==0:
+        #         num//=2
+        #     elif num%3==0:
+        #         num//=3
+        #     elif num%5==0:
+        #         num//=5
+        #     else:
+        #         return False
+        # return True
