255f5675434656c637a00caa04fa45bc580b8a24
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-11-27 08:53:00 +0100

    Update 0041. First Missing Positive.py

--- Python3/0041. First Missing Positive.py
+++ Python3/0041. First Missing Positive.py
@@ -1,29 +1,41 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
+        for i in range(n):
+            while 1<=nums[i]<=n and nums[i]!=nums[nums[i]-1]:
+                index = nums[i]-1
+                nums[index],nums[i] = nums[i],nums[index]
+        
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        
+        return n + 1
 
-        if 1 not in nums:
-            return 1
+        # n = len(nums)
+
+        # if 1 not in nums:
+        #     return 1
         
-        if n==1:
-            return 2
+        # if n==1:
+        #     return 2
         
-        for i in range(n):
-            if nums[i]<=0 or nums[i]>n:
-                nums[i] = 1
+        # for i in range(n):
+        #     if nums[i]<=0 or nums[i]>n:
+        #         nums[i] = 1
         
-        for i in range(n):
-            index = abs(nums[i])
-            if index==n:
-                nums[0] = -abs(nums[0])
-            else:
-                nums[index] = -abs(nums[index])
+        # for i in range(n):
+        #     index = abs(nums[i])
+        #     if index==n:
+        #         nums[0] = -abs(nums[0])
+        #     else:
+        #         nums[index] = -abs(nums[index])
         
-        for i in range(1,n):
-            if nums[i]>0:
-                return i
+        # for i in range(1,n):
+        #     if nums[i]>0:
+        #         return i
         
-        if nums[0]>0:
-            return n
-        else:
-            return n + 1
+        # if nums[0]>0:
+        #     return n
+        # else:
+        #     return n + 1
