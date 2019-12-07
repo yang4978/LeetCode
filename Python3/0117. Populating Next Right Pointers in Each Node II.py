c73662c636338097d752b9afa7230b3d73dd2634
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-12-07 11:47:32 +0100

    Create 0117. Populating Next Right Pointers in Each Node II.py

---
+++ Python3/0117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+
+        queue = [(root,0)]
+
+        while queue:
+            node,layer = queue.pop(0)
+            if not queue or queue[0][1]!=layer:
+                node.next = None
+            else:
+                node.next = queue[0][0]
+            
+            if node.left:
+                queue.append((node.left,layer+1))
+            
+            if node.right:
+                queue.append((node.right,layer+1))
+        
+        return root
