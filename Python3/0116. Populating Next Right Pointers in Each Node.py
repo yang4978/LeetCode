861eaa8e4977b8b7e23928572c09feda13d57556
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-12-07 11:41:41 +0100

    Update 0116. Populating Next Right Pointers in Each Node.py

--- Python3/0116. Populating Next Right Pointers in Each Node.py
+++ Python3/0116. Populating Next Right Pointers in Each Node.py
@@ -1,16 +1,46 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
 class Solution:
-    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+    def connect(self, root: 'Node') -> 'Node':
         if not root:
-            return 0
-        
-        if root.left and not root.left.left and not root.left.right:
-            return self.sumOfLeftLeaves(root.left)+self.sumOfLeftLeaves(root.right)+root.left.val
+            return root
 
-        return self.sumOfLeftLeaves(root.left)+self.sumOfLeftLeaves(root.right)
+        left = root.left
+        right = root.right
+        
+        while left:
+            left.next = right
+            left = left.right
+            right = right.left
+        
+        self.connect(root.left)
+        self.connect(root.right)
+
+        return root
+
+        # if not root:
+        #     return root
+
+        # queue = [(root,0)]
+
+        # while queue:
+        #     node,layer = queue.pop(0)
+        #     if not queue or queue[0][1]!=layer:
+        #         node.next = None
+        #     else:
+        #         node.next = queue[0][0]
+            
+        #     if node.left:
+        #         queue.append((node.left,layer+1))
+            
+        #     if node.right:
+        #         queue.append((node.right,layer+1))
+        
+        # return root
