03e103bd720156f08440906f412db44d50ac2925
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-12-21 22:25:53 +0100

    Update 0556. Next Greater Element III.py

--- Python3/0556. Next Greater Element III.py
+++ Python3/0556. Next Greater Element III.py
@@ -1,5 +1,33 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
+        # stack = []
+        # res = 0
+        # a = []
+        # l = 0
+
+        # while n:
+        #     a.append(n%10)
+        #     n = n//10
+        #     l += 1
+        # a.reverse()
+
+        # for i in range(l-1,-1,-1):
+        #     if stack and stack[-1]>a[i]:
+        #         for j in range(len(stack)):
+        #             if stack[j]>a[i]:
+        #                 stack[j],a[i] = a[i],stack[j]
+        #                 break
+
+        #         a = a[:i]+[a[i]]+stack
+        
+        #         for i in range(l):
+        #             n = n*10 + a[i]
+        #         return n if n<=2**31-1 else -1
+        #     stack.append(a[i])
+        
+        # return -1
+
+
         stack = []
         res = 0
 
@@ -18,6 +46,5 @@ class Solution:
                 n = n*pow(10,digit+1)+t*pow(10,digit)+res
                 return n if n<=2**31-1 else -1
             stack.append(t)
-            stack.sort()
         
         return -1
