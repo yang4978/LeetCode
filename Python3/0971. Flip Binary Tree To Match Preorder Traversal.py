559b5b2030fdbdbdfe1ac6e9cb23ed45f742f0b3
yang4978 <40636563+yang4978@users.noreply.github.com>
2020-01-20 11:46:35 +0100

    Update 0971. Flip Binary Tree To Match Preorder Traversal.py

--- Python3/0971. Flip Binary Tree To Match Preorder Traversal.py
+++ Python3/0971. Flip Binary Tree To Match Preorder Traversal.py
@@ -7,25 +7,59 @@
 
 class Solution:
     def flipMatchVoyage(self, root: TreeNode, voyage: List[int]) -> List[int]:
-        self.i = 0
-        self.res = []
+        # self.i = 0
+        # self.res = []
 
-        def dfs(node):
-            if not node:
-                return
-            if node.val != voyage[self.i]:
-                self.res = [-1]
-                return
+        # def dfs(node):
+        #     if not node:
+        #         return
+        #     if node.val != voyage[self.i]:
+        #         self.res = [-1]
+        #         return
             
-            self.i += 1
-            if self.i < len(voyage) and node.left and node.left.val != voyage[self.i]:
-                self.res.append(node.val)
-                dfs(node.right)
-                dfs(node.left)
-            else:
-                dfs(node.left)
-                dfs(node.right)
+        #     self.i += 1
+        #     if self.i < len(voyage) and node.left and node.left.val != voyage[self.i]:
+        #         self.res.append(node.val)
+        #         dfs(node.right)
+        #         dfs(node.left)
+        #     else:
+        #         dfs(node.left)
+        #         dfs(node.right)
         
-        dfs(root)
+        # dfs(root)
 
-        return self.res
+        # return self.res
+
+        res,stack = [],[]
+        i = 0
+        pre = TreeNode(-1)
+        
+        while root:
+            if root.val == voyage[i]:
+                i += 1
+                stack.append(root)
+                pre = root
+                root = root.left
+            elif not pre.right or (pre.right and pre.right.val == voyage[i]):
+                    res.append(pre.val)
+                    root = pre.right
+                    pre.right = pre.left
+            else:
+                return [-1]
+
+        while stack:
+            pre = stack.pop()
+            node = pre.right
+            while node:
+                if node.val == voyage[i]:
+                    i += 1
+                    stack.append(node)
+                    pre = node
+                    node = node.left
+                elif (not pre.right) or (pre.right and pre.right.val==voyage[i]):
+                    res.append(pre.val)
+                    node = pre.right
+                    pre.right = pre.left
+                else:
+                    return [-1]
+        return res
