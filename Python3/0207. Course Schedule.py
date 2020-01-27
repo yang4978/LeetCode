39da00bc4e2652758b0fd4ac4c6933485370eabb
yang4978 <40636563+yang4978@users.noreply.github.com>
2020-01-27 07:54:51 +0100

    Update 0207. Course Schedule.py

--- Python3/0207. Course Schedule.py
+++ Python3/0207. Course Schedule.py
@@ -1,17 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-
         def dfs(course,index,used):           
             res = True
 
-            for i in course[index]:
-                if used[i] == 1:
+            if used[index] == -1:
+                return True
+            
+            if used[index] == 1:
+                return False
+            
+            used[index] = 1
+
+            for i in course[index]:                
+                if dfs(course,i,used) == False:
                     return False
-                else:
-                    used[i] = 1
-                    if dfs(course,i,used) == False:
-                        return False
-                    used[i] = 0
+            
+            used[index] = -1
             
             return res
 
@@ -28,3 +32,34 @@ class Solution:
                 return False
 
         return res
+        
+
+
+
+#         def dfs(course,index,used):           
+#             res = True
+
+#             for i in course[index]:
+#                 if used[i] == 1:
+#                     return False
+#                 else:
+#                     used[i] = 1
+#                     if dfs(course,i,used) == False:
+#                         return False
+#                     used[i] = 0
+            
+#             return res
+
+#         course = collections.defaultdict(set)
+
+#         for i,j in prerequisites:
+#             course[i] |= {j}
+        
+#         used = [0]*numCourses
+
+#         res = True
+#         for i in range(numCourses):
+#             if dfs(course,i,used) == False:
+#                 return False
+
+#         return res
