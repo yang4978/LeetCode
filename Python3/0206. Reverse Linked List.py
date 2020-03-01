f59fc06abb670ac6a468b83f472cc395bbf57a7b
yang4978 <40636563+yang4978@users.noreply.github.com>
2020-03-01 17:52:09 +0100

    Update 0206. Reverse Linked List.py

--- Python3/0206. Reverse Linked List.py
+++ Python3/0206. Reverse Linked List.py
@@ -6,17 +6,44 @@
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        new_head = None
-        while(head):
-            tmp = head.next
-            head.next = new_head
-            new_head = head
-            head = tmp
-        return new_head
-        # new_head = None
-        # while(head):
-        #     temp = ListNode(head.val)
-        #     temp.next = new_head
-        #     head = head.next
-        #     new_head = temp
-        # return new_head
+        ###递归法###
+        if not head or not head.next:
+            return head
+        
+        h1 = self.reverseList(head.next)
+
+        head.next.next = head
+        
+        head.next = None
+
+        return h1
+
+        ###递归法 笨办法###
+        # if not head or not head.next:
+        #     return head
+        
+        # h1 = self.reverseList(head.next)
+
+        # head.next = None
+        
+        # node = h1
+        # while node.next:
+        #     node = node.next
+        
+        # node.next = head
+
+        # return h1
+
+
+        ###迭代法###
+        # new_head = None                # 新头部
+        # cur = head                     # 遍历原链表所用的节点
+
+        # while cur:                     # 遍历链表
+        #     t = cur.next               # 保存cur节点在原来链表中的下一个节点
+        #     cur.next = new_head        # 由于每次新遍历的节点都是new_head的上位节点
+        #                                # 因此cur.next = new_head    
+        #     new_head = cur             # 重新把new_head移到头部的位置，即cur所在的位置
+        #     cur = t                    # 让cur转移到原来链表的下一个节点，继续遍历
+        
+        # return new_head                # 最终返回new_head
