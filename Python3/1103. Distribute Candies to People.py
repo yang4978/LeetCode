3e66302e2df1d418f2c844dd83b790f89a3025d6
yang4978 <40636563+yang4978@users.noreply.github.com>
2020-03-05 16:25:35 +0100

    Update 1103. Distribute Candies to People.py

--- Python3/1103. Distribute Candies to People.py
+++ Python3/1103. Distribute Candies to People.py
@@ -1,21 +1,45 @@
 class Solution:
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
-        k = 0
+        n = num_people
 
-        while candies > k*num_people*num_people + num_people*(num_people+1)//2:
-            candies -= k*num_people*num_people + num_people*(num_people+1)//2
-            k += 1
+        # k = 0
+
+        # while candies > k*n*n + n*(n+1)//2:
+        #     candies -= k*n*n + n*(n+1)//2
+        #     k += 1
+
+        k = int((-1/2 + math.sqrt(1/4+2*candies))/(n))
+
+        candies -= k*(k-1)*n*n//2 + k*n*(n+1)//2
         
-        res = [k*(k-1)*num_people//2+(i+1)*k for i in range(num_people)] if k else [0]*num_people
+        res = [k*(k-1)*n//2+(i+1)*k for i in range(n)] if k else [0]*n
 
-        index = 0
-        while candies:
-            if candies >= k*num_people + index + 1:
-                candies -= k*num_people + index + 1
-                res[index] += k*num_people + index + 1
-                index += 1
+        for i in range(n):
+            if candies >= k*n + i + 1:
+                candies -= k*n + i + 1
+                res[i] += k*n + i + 1
             else:
-                res[index] += candies
+                res[i] += candies
                 break
             
         return res
+
+        # n = num_people
+
+        # k = 0
+
+        # while candies > k*n*n + n*(n+1)//2:
+        #     candies -= k*n*n + n*(n+1)//2
+        #     k += 1
+
+        # index = 0
+        # while candies:
+        #     if candies >= k*n + index + 1:
+        #         candies -= k*n + index + 1
+        #         res[index] += k*n + index + 1
+        #         index += 1
+        #     else:
+        #         res[index] += candies
+        #         break
+            
+        # return res
