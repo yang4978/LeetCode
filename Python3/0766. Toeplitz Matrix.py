257920d08b74564d62a2b23d899e70d10e8fdad0
yang4978 <40636563+yang4978@users.noreply.github.com>
2020-04-07 18:50:28 +0800

    Create 0766. Toeplitz Matrix.py

---
+++ Python3/0766. Toeplitz Matrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        m = len(matrix)
+        if not m:
+            return True
+        n = len(matrix[0])
+    
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][j] != matrix[i-1][j-1]:
+                    return False
+        
+        return True
