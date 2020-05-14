52dcf5735e7e7e0827046d4fed21286ee46a9e87
yang4978 <40636563+yang4978@users.noreply.github.com>
2020-05-14 08:47:44 +0800

    Update 0136. Single Number.py

--- Python3/0136. Single Number.py
+++ Python3/0136. Single Number.py
@@ -1,6 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        for i in nums:
-            res ^= i
-        return res
+        # res = 0
+        # for i in nums:
+        #     res ^= i
+        
+        # return res
+
+        return functools.reduce(lambda x,y: x^y,nums)
