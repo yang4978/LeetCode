42432837099be74cf56d28b307dd02c7957b23d2
yang4978 <40636563+yang4978@users.noreply.github.com>
2020-08-23 10:28:13 +0200

    Create 0201. Bitwise AND of Numbers Range.py

---
+++ Python3/0201. Bitwise AND of Numbers Range.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        # shift = 0
+        # while m < n:
+        #     m >>= 1
+        #     n >>= 1
+        #     shift += 1
+        
+        # return m << shift
+
+        while m < n:
+            n &= (n-1)
+        
+        return n
