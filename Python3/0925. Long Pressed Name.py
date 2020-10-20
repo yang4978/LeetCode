bd5ca9e300d494d4be38f949ef1460922aad6e8b
yang4978 <40636563+yang4978@users.noreply.github.com>
2020-10-20 18:19:33 +0200

    Update 0925. Long Pressed Name.py

--- Python3/0925. Long Pressed Name.py
+++ Python3/0925. Long Pressed Name.py
@@ -1,21 +1,43 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
         i,j = 0,0
-        x = len(name)
-        y = len(typed)
-        while(i<x and j<y):
-            if(name[i]==typed[j]):
+
+        while j<len(typed):
+            if i < len(name) and name[i] == typed[j]:
                 i += 1
                 j += 1
-            elif(typed[j]==typed[j-1]):
+            elif j > 0 and typed[j] == typed[j-1]:
                 j += 1
             else:
                 return False
+        
+        return i == len(name)
 
-        if(j==y and i<x):
-            return False
-        else:
-            return True
+        # i,j = 0,0
+        # x = len(name)
+        # y = len(typed)
+        # while(i<x and j<y):
+        #     if(name[i]==typed[j]):
+        #         i += 1
+        #         j += 1
+        #     elif(typed[j]==name[max(0,i-1)]):
+        #         j += 1
+        #     else:
+        #         return False
+
+        # if i == x:
+        #     while j<y:
+        #         if typed[j] == name[i-1]:
+        #             j += 1
+        #         else:
+        #             return False
+
+        # if(j==y and i<x):
+        #     return False
+        # else:
+        #     return True
+
+
         # y = len(typed)
         # x = len(name)
         
