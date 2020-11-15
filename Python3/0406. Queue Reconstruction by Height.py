b9c3cb82eec5fc575c8162256102bb0d76c1964f
yang4978 <40636563+yang4978@users.noreply.github.com>
2020-11-15 19:14:08 +0100

    Update 0406. Queue Reconstruction by Height.py

--- Python3/0406. Queue Reconstruction by Height.py
+++ Python3/0406. Queue Reconstruction by Height.py
@@ -1,35 +1,41 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        height = set();
-        height_block = collections.defaultdict(list);
-        queue = [];
-        order = collections.defaultdict(int)
+        person = sorted(people, key=lambda x: (-x[0],x[1]))
+        res = []
+        for pair in person:
+            res.insert(pair[1],pair)
+        return res
 
-        for pair in people:
-            h,k = pair
-            height.add(h)
-            height_block[h].append(pair)
-            order[h] = 0
-        
-        height = sorted(list(height))
+        # height = set();
+        # height_block = collections.defaultdict(list);
+        # queue = [];
+        # order = collections.defaultdict(int)
 
-        for h in height:
-            height_block[h].sort(key=lambda x:-x[1])
+        # for pair in people:
+        #     h,k = pair
+        #     height.add(h)
+        #     height_block[h].append(pair)
+        #     order[h] = 0
         
-        print(height_block)
-        
-        while len(queue)<len(people): 
-            last_num = -1
-            for i in range(len(height)):
-                h = height[i]
-                num = order[h]
-                if num<=last_num:
-                    break
+        # height = sorted(list(height))
 
-                if height_block[h]!=[] and height_block[h][-1][1] == num:
-                    queue.append(height_block[h].pop())
-                    last_num = num
-                    for j in range(i+1):
-                        order[height[j]] += 1
+        # for h in height:
+        #     height_block[h].sort(key=lambda x:-x[1])
+        
+        # print(height_block)
+        
+        # while len(queue)<len(people): 
+        #     last_num = -1
+        #     for i in range(len(height)):
+        #         h = height[i]
+        #         num = order[h]
+        #         if num<=last_num:
+        #             break
+
+        #         if height_block[h]!=[] and height_block[h][-1][1] == num:
+        #             queue.append(height_block[h].pop())
+        #             last_num = num
+        #             for j in range(i+1):
+        #                 order[height[j]] += 1
             
-        return queue
+        # return queue
