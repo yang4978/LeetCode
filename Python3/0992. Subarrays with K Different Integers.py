7ac6410c23a5d9ea2f99ed4cc8e14343927880fe
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-02-09 09:36:20 +0800

    Update 0992. Subarrays with K Different Integers.py

--- Python3/0992. Subarrays with K Different Integers.py
+++ Python3/0992. Subarrays with K Different Integers.py
@@ -1,35 +1,37 @@
-class Window:
-    def __init__(self):
-        self.num = collections.Counter()
-        self.non_dup = 0
-        
-    def add(self,s):
-        self.num[s] += 1
-        if self.num[s]==1 :
-            self.non_dup += 1
-            
-    def sub(self,s):
-        self.num[s] -= 1
-        if self.num[s]==0 :
-            self.non_dup -= 1
-
 class Solution:
     def subarraysWithKDistinct(self, A: List[int], K: int) -> int:
-        W1 = Window()
-        W2 = Window()
-        l1=l2=ans = 0
-        
-        
-        for x in A:
-            W1.add(x)
-            W2.add(x)
+        l = len(A)
+        res = 0
+        total1 = 0
+        total2 = 0
+        num1 = collections.defaultdict(int)
+        num2 = collections.defaultdict(int)
+        left1 = 0
+        left2 = 0
+
+        for right in range(l):
+            n = A[right]
+
+            if num1[n] == 0:                
+                total1 += 1
+            if num2[n] == 0:
+                total2 += 1
+
+            num1[n] += 1
+            num2[n] += 1
             
-            while W1.non_dup >K : 
-                W1.sub(A[l1])
-                l1 += 1
-            while W2.non_dup >= K:
-                W2.sub(A[l2])
-                l2 += 1
+            while total1 > K-1:
+                num1[A[left1]] -= 1
+                if num1[A[left1]] == 0:
+                    total1 -= 1
+                left1 += 1
             
-            ans += l2-l1
-        return ans
+            while total2 > K:
+                num2[A[left2]] -= 1
+                if num2[A[left2]] == 0:
+                    total2 -= 1
+                left2 += 1
+            
+            res += left1 - left2
+        
+        return res
