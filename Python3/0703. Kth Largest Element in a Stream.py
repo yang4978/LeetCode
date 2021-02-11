7a4612ac6c204fab3883f0b3d999eaa318a77369
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-02-11 10:20:25 +0800

    Create 0703. Kth Largest Element in a Stream.py

---
+++ Python3/0703. Kth Largest Element in a Stream.py
@@ -0,0 +1,15 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.arr = nums
+        self.k = k
+
+    def add(self, val: int) -> int:
+        self.arr.append(val)
+        self.arr.sort(reverse=True)
+        return self.arr[self.k-1]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
