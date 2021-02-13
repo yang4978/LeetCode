aa55a647d9fd46a70dcf4195fb8813eeafcbe3d1
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-02-13 11:54:22 +0800

    Update 0448. Find All Numbers Disappeared in an Array.py

--- Python3/0448. Find All Numbers Disappeared in an Array.py
+++ Python3/0448. Find All Numbers Disappeared in an Array.py
@@ -1,17 +1,26 @@
+# class Solution:
+#     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+#         def swap(nums,i,j):
+#             if i==j:
+#                 return
+#             nums[i] = nums[i]^nums[j]
+#             nums[j] = nums[i]^nums[j]
+#             nums[i] = nums[i]^nums[j]
+
+#         n = len(nums)
+
+#         for i in range(n):
+#             while nums[nums[i]-1]!=nums[i]:
+#                 swap(nums,nums[i]-1,i)
+#                 #nums[nums[i]-1],nums[i] = nums[i],nums[nums[i]-1]
+        
+#         return [ i+1 for i in range(n) if nums[i] != (i+1)]
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        def swap(nums,i,j):
-            if i==j:
-                return
-            nums[i] = nums[i]^nums[j]
-            nums[j] = nums[i]^nums[j]
-            nums[i] = nums[i]^nums[j]
-
         n = len(nums)
-
-        for i in range(n):
-            while nums[nums[i]-1]!=nums[i]:
-                swap(nums,nums[i]-1,i)
-                #nums[nums[i]-1],nums[i] = nums[i],nums[nums[i]-1]
+        for num in nums:
+            x = (num-1)%n
+            nums[x] += n
         
-        return [ i+1 for i in range(n) if nums[i] != (i+1)]
+        return [i+1 for i in range(n) if nums[i]<=n]
