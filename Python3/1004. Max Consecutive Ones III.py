bcc3671d0c16097cd30d98a8cb3534dd57c3640e
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-02-19 08:04:39 +0800

    Update 1004. Max Consecutive Ones III.py

--- Python3/1004. Max Consecutive Ones III.py
+++ Python3/1004. Max Consecutive Ones III.py
@@ -1,27 +1,42 @@
+# class Solution:
+#     def longestOnes(self, A: List[int], K: int) -> int:
+#         res = 0
+#         num_k = 0
+#         diff = 0
+#         temp_sum = 0
+#         cir = [0]
+
+#         for i in A:
+#             if i == 0:
+#                 cir.append(diff)
+#                 num_k += 1
+#                 diff = 0
+#                 if num_k >= K:
+#                     res = max(res,temp_sum + K)
+#                     temp_sum -= cir[num_k-K]
+#             else:
+#                 diff += 1
+#                 temp_sum += 1 
+
+#         if num_k < K:
+#             res = temp_sum + num_k
+#         else:
+#             res = max(res,temp_sum + K)
+
+#         return res
+
 class Solution:
     def longestOnes(self, A: List[int], K: int) -> int:
-        res = 0
-        num_k = 0
-        diff = 0
-        temp_sum = 0
-        cir = [0]
+        l = 0
+        r = 0
+        n = len(A)
+        zero = 0
+        while r < n:
+            zero += 1 if A[r] == 0 else 0
+            r += 1
+                
+            if zero > K:
+                zero -= 1 if A[l] == 0 else 0
+                l += 1
 
-        for i in A:
-            if i == 0:
-                cir.append(diff)
-                num_k += 1
-                diff = 0
-                if num_k >= K:
-                    res = max(res,temp_sum + K)
-                    temp_sum -= cir[num_k-K]
-            else:
-                diff += 1
-                temp_sum += 1 
-
-        if num_k < K:
-            res = temp_sum + num_k
-        else:
-            res = max(res,temp_sum + K)
-
-        return res
-
+        return r-l
