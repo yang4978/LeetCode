bce2297d5a28c8a23c9a46f8db9edca471a03a55
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-03-05 08:40:05 +0800

    Update 0338. Counting Bits.py

--- Python3/0338. Counting Bits.py
+++ Python3/0338. Counting Bits.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type num: int
         :rtype: List[int]
         """
-        res = [0]*(num+1)
+#         res = [0]*(num+1)
 
-        for i in range(1,num+1):
-            res[i] = (res[i-1]+1) if i%2 else res[i//2]
+#         for i in range(1,num+1):
+#             res[i] = (res[i-1]+1) if i%2 else res[i//2]
         
-        return res
+#         return res
 
         # res = [0]*(num+1)
 
@@ -20,3 +20,12 @@ class Solution(object):
         #     mask <<= 1
 
         # return res
+        
+        arr = [0]
+        bit = 1
+        while((1<<bit)<num):
+            bit += 1
+        for i in range(1,bit+1):
+            for j in range(1<<(i-1)):
+                arr.append(1+arr[j])
+        return arr[:num+1] if len(arr)>num else arr+[1]
