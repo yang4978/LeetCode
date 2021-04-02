1a1b11e01f9d1c86af47249057b7d3f6b9469768
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-04-02 09:21:56 +0800

    Update 面试题 17.21. 直方图的水量.py

--- Python3/面试题 17.21. 直方图的水量.py
+++ Python3/面试题 17.21. 直方图的水量.py
@@ -5,7 +5,7 @@ class Solution:
 
         for h in height:
             if(h >= wall[0]):
-                edge = min(h, wall[0])
+                edge = wall[0]
                 while(wall):
                     res += edge - wall.pop() 
             wall.append(h)
@@ -14,7 +14,7 @@ class Solution:
         while(wall):
             h = wall.pop()
             if(h >= rwall[0]):
-                edge = min(h, rwall[0])
+                edge = rwall[0]
                 while(rwall):
                     res += edge - rwall.pop() 
             rwall.append(h)
