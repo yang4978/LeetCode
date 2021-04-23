6c8c9d7d5e29a85979d00c631a749b95f657930c
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-04-23 08:40:42 +0800

    Update 0368. Largest Divisible Subset.py

--- Python3/0368. Largest Divisible Subset.py
+++ Python3/0368. Largest Divisible Subset.py
@@ -1,12 +1,35 @@
+# class Solution:
+#     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+#         nums.sort()
+#         dp = [[i] for i in nums]
+#         res = []
+#         for i in range(len(nums)):
+#             for j in range(i):
+#                 if nums[i]%nums[j]==0 and len(dp[j])+1>len(dp[i]):
+#                     dp[i] = dp[j]+nums[i:i+1]
+#             if len(dp[i])>len(res):
+#                 res = dp[i]
+#         return res
+
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
-        dp = [[i] for i in nums]
-        res = []
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[i]%nums[j]==0 and len(dp[j])+1>len(dp[i]):
-                    dp[i] = dp[j]+nums[i:i+1]
-            if len(dp[i])>len(res):
-                res = dp[i]
+        l = len(nums)
+        dp = [1] * l
+
+        for i in range(1, l):
+            for j in range(i - 1, - 1 ,-1):
+                if nums[i] % nums[j] == 0:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        target = max(dp)
+        i = l - 1
+        res = [0] * target
+        while target:
+            if(dp[i] == target and (target == len(res) or res[target] % nums[i] == 0)):
+                res[target - 1] = nums[i]
+                target -= 1
+            i -= 1
+        
         return res
+
