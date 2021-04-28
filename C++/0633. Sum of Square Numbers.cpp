ef769d70ef3b37c025fcda9b47254f78f3b196a5
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-04-28 08:29:04 +0800

    Create 0633. Sum of Square Numbers.cpp

---
+++ C++/0633. Sum of Square Numbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(long i = 0; i * i <= c; ++i) {
+            double b = sqrt(c - i * i);
+            if(b == (int)b) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
