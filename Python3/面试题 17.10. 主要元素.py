1754097fcb061b6a1ef4904f153f373980105084
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-07-09 16:23:48 +0800

    Update 面试题 17.10. 主要元素.py

--- Python3/面试题 17.10. 主要元素.py
+++ Python3/面试题 17.10. 主要元素.py
@@ -1,5 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        x = nums[len(nums)//2]
-        return x if nums.count(x) > len(nums) // 2 else -1
+        # nums.sort()
+        # x = nums[len(nums)//2]
+        # return x if nums.count(x) > len(nums) // 2 else -1
+
+        count = 0
+        major = 0
+        for n in nums:
+            if count == 0:
+                major = n
+            count += (1 if n == major else -1)
+
+        count = 0
+        
+        for n in nums:
+            if(n == major):
+                count += 1
+        
+        return (major if count > len(nums) // 2 else -1)
