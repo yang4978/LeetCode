6387d05b619d403414bad273fc3a7a2c58668db7
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-12-28 20:28:28 +0800

    Update 1624. Largest Substring Between Two Equal Characters.py

--- Python3/1624. Largest Substring Between Two Equal Characters.py
+++ Python3/1624. Largest Substring Between Two Equal Characters.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
-        letter = collections.defaultdict(list)
-        for i, c in enumerate(s):
-            letter[c].append(i)
-        
+        letter = dict()
         res = -1
-        for arr in letter.values():
-            res = max(res, arr[-1] - arr[0] - 1)
+        for i, c in enumerate(s):
+            if c not in letter:
+                letter[c] = i
+            else:
+                res = max(res, i - letter[c] - 1)
         
         return res
